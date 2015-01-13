579c1911c826bfda17d96c1641dca1dbd578909a
帅帅 <347212291@qq.com>
2015-01-13 16:15:19 +0800

    Update yanzhengma.rb
    增加了图形去噪，识别率大大提升

--- yanzhengma.rb
+++ yanzhengma.rb
@@ -1,8 +1,10 @@
-require 'rubygems'
+# encoding: utf-8
 require 'rtesseract'
 require 'mini_magick'
 require 'RMagick'
-include Magick
+
+=begin
+#first try
 
 img = MiniMagick::Image.new("artist.png")   
 img.colorspace("GRAY")#灰度化
@@ -11,8 +13,11 @@ img.negate
 #img.monochrome
 image = RTesseract.new(img.path)  
 p image.to_s.sub(/\s+$/, "") # => "254369"   
+=end 
 
 =begin
+#second try
+
 require 'tesseract'
 
 e = Tesseract::Engine.new {|e|
@@ -28,3 +33,22 @@ thumb.crop_resized!(120, 120, Magick::CenterGravity)
 
 p e.text_for('artist.png').strip # => 'ABC'
 =end
+
+#third try
+
+img = Magick::Image.read('1.jpg').first
+#img = Magick::Image.read('http://lailaihui.com/Customer/GetValidateCode?v=0.4715193349402398').first
+img = img.reduce_noise(1)
+img.resize!(6)
+img.write('tmp.jpg')
+
+img = MiniMagick::Image.new("tmp.jpg")
+img.crop("#{img[:width] - 2}x#{img[:height] - 2}+1+1")
+# img.monochrome # 去色
+img.colorspace("GRAY") #灰度化
+# img.monochrome #二值化
+image = RTesseract.new(img.path,:lang => "eng", options: [:digits, :quiet]) #ocr识别
+p image.to_s.sub(/\s+$/, "") #=> "5692"ocr识别
+
+# delete tmp file
+File.delete("tmp.jpg")
